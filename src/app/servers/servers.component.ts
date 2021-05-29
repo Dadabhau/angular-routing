5d37bfa1d5aa00066e03deea3059e1915f962ae8
dadabhau <dadabhaut@ithena.ai>
2021-05-29 15:00:55 +0530

    Routing Completed

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,26 +1,26 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { ServersService } from "./servers.service";
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: "app-servers",
-  templateUrl: "./servers.component.html",
-  styleUrls: ["./servers.component.css"],
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  public servers: { id: number; name: string; status: string }[] = [];
+  private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(
-    private serversService: ServersService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
+
   onReload() {
-    //Using Relative Paths in Programmatic Navigation
-    //this.router.navigate(["servers"], { relativeTo: this.route });
+    // this.router.navigate(['servers'], {relativeTo: this.route});
   }
+
 }
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,35 +1,34 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
-// Important Note about Route Observables
-import { Subscription } from "rxjs/Subscription";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
-  selector: "app-user",
-  templateUrl: "./user.component.html",
-  styleUrls: ["./user.component.css"],
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit, OnDestroy {
-  user: { id: number; name: string };
+  user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  //Important Note about Route Observables
-  paramsSubscripction: Subscription;
-
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    // Fetching Route Parameters
     this.user = {
-      id: this.route.snapshot.params["id"],
-      name: this.route.snapshot.params["name"],
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
     };
-
-    //Fetching Route Parameters Reactively
-    this.paramsSubscripction = this.route.params.subscribe((params: Params) => {
-      (this.user.id = params["id"]), (this.user.name = params["name"]);
-    });
+    this.paramsSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
   ngOnDestroy() {
-    this.paramsSubscripction.unsubscribe();
+    this.paramsSubscription.unsubscribe();
   }
+
 }
